e97a0826e5f4fb4e59a825ce8e7b42853393320b
Dennis Tismenko <dtismenkodeveloper@gmail.com>
2020-01-14 18:16:16 -0500

    Remove colour dependency from Page component and refactor

--- src/root/Root.react.jsx
+++ src/root/Root.react.jsx
@@ -9,35 +9,35 @@ const Root = () => {
     <FullPage>
       <FullPageSection direction="vertical">
         <Page path="aqua" color="aqua">
-          <div className={classes.Page}>
+          <div className={[classes.Page, classes['bg-aqua']].join(' ')}>
             <h1>Aqua</h1>
           </div>
         </Page>
         <FullPageSection direction="horizontal">
           <Page path="beige" color="beige">
-            <div className={classes.Page}>
+            <div className={[classes.Page, classes['bg-beige']].join(' ')}>
               <h1>Beige</h1>
             </div>
           </Page>
           <Page path="crimson" color="crimson">
-            <div className={classes.Page}>
+            <div className={[classes.Page, classes['bg-crimson']].join(' ')}>
               <h1>Crimson</h1>
             </div>
           </Page>
           <FullPageSection direction="vertical">
             <Page path="green" color="green">
-              <div className={classes.Page}>
+              <div className={[classes.Page, classes['bg-green']].join(' ')}>
                 <h1>Green</h1>
               </div>
             </Page>
             <Page path="blue" color="blue">
-              <div className={classes.Page}>
+              <div className={[classes.Page, classes['bg-blue']].join(' ')}>
                 <h1>Blue</h1>
               </div>
             </Page>
           </FullPageSection>
           <Page path="yellow" color="yellow">
-            <div className={classes.Page}>
+            <div className={[classes.Page, classes['bg-yellow']].join(' ')}>
               <h1>Yellow</h1>
             </div>
           </Page>
